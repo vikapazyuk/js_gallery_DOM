762f6f9c6f3b2a82f5e94f67c318d7b8ae04c63e
Viktoriia <vikapazyuk@gmail.com>
2025-01-20 15:03:02 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-'use strict';
+const largeImg = document.querySelector('.gallery__large-img');
+const gallery = document.querySelector('.gallery__list');
+
+gallery.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let link;
+
+  if (e.target.tagName === 'IMG') {
+    link = e.target.closest('a');
+  } else if (e.target.tagName === 'A') {
+    link = e.target;
+  }
+
+  if (link) {
+    largeImg.src = link.href;
+  }
+});
