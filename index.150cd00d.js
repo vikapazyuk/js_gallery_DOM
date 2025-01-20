f18bbde137e787c40ac2a0df9c32bc4eae17dc79
Viktoriia <vikapazyuk@gmail.com>
2025-01-20 16:35:59 +0200

    make build

---
+++ index.150cd00d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){var r;t.preventDefault(),"IMG"===t.target.tagName?r=t.target.closest("a"):"A"===t.target.tagName&&(r=t.target),r&&(e.src=r.href)});
+//# sourceMappingURL=index.150cd00d.js.map
